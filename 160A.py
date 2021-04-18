9aa94007ffe99ccaca123de7a22bbc880921c184
naveen rajan <rajanice1996@gmail.com>
2021-04-18 15:15:09 +0530

    Twins

--- 160A.py
+++ 160A.py
@@ -1,14 +1,14 @@
 n = int(input())
-l = list(map(int,input().split()))
+l = list(map(int, input().split()))
 l1 = sorted(l)
 s = sum(l)
-csum = 0
+c_sum = 0
 count = 0
-for i in range(len(l1)-1,-1,-1):
+for i in range(len(l1)-1, -1, -1):
     x = l1[i]
-    if csum<=(s//2) :
-        csum+=x
-        count+=1
+    if c_sum <= (s//2):
+        c_sum += x
+        count += 1
     else:
         break
 print(count)
