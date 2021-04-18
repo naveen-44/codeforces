5a95051256d2a447a5d55fbec891a82b3ceb0750
naveen rajan <rajanice1996@gmail.com>
2021-04-18 11:44:00 +0530

    Lucky division

---
+++ 122A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include<cstring>
+using namespace std;
+ 
+int main() {
+	int n;
+	int a[14]={4,7,44,47,74,77,444,447,474,477,744,747,774,777};
+	cin>>n;
+	for(int i=0;i<14;i++){
+		if(n%a[i]==0){
+			cout<<"YES";
+			return 0;
+		}
+	}
+	cout<<"NO";
+return 0;	
+}
